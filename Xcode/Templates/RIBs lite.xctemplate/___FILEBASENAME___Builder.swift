91304507d5ff0ae1780cdce67e90562d61f579ac
Dmitry Savitskiy <dmitry.savitskiy@icloud.com>
2023-07-09 22:22:51 +0300

    Updated apps configuration: xcode template

--- Xcode/Templates/RIBs lite.xctemplate/___FILEBASENAME___Builder.swift
+++ Xcode/Templates/RIBs lite.xctemplate/___FILEBASENAME___Builder.swift
@@ -4,10 +4,7 @@ import UIKit
 
 final class ___VARIABLE_productName___Builder {
     func build(appComponent: AppComponent) -> UIViewController {
-        guard let vc = R.storyboard.___VARIABLE_productName___.___VARIABLE_productName___ViewController() else {
-            fatalError("Unable to instantiate ___VARIABLE_productName___VC")
-        }
-        
+        let vc = StoryboardScene.___VARIABLE_productName___.___VARIABLE_productName___ViewControllerId.instantiate()
         let router = ___VARIABLE_productName___Router(appComponent: appComponent, viewController: vc)
         let interactor = ___VARIABLE_productName___Interactor(presenter: vc, router: router)
         vc.interactor = interactor
