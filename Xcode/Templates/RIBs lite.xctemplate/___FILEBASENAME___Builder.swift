a312bae4a9dbc020e75f245c81db1b80941b6327
Dmitry Savitskiy <dmitry.savitskiy@icloud.com>
2025-02-03 00:22:27 +0300

    Updated apps configuration: Dash
    Update Xcode tempalates

--- Xcode/Templates/RIBs lite.xctemplate/___FILEBASENAME___Builder.swift
+++ Xcode/Templates/RIBs lite.xctemplate/___FILEBASENAME___Builder.swift
@@ -3,12 +3,12 @@
 import UIKit
 
 final class ___VARIABLE_productName___Builder {
-    func build(appComponent: AppComponent) -> UIViewController {
-        let vc = StoryboardScene.___VARIABLE_productName___.___VARIABLE_productName___ViewControllerId.instantiate()
-        let router = ___VARIABLE_productName___Router(appComponent: appComponent, viewController: vc)
-        let interactor = ___VARIABLE_productName___Interactor(presenter: vc, router: router)
-        vc.interactor = interactor
+    func build(appComponent: AppComponent) -> ViewController {
+        let viewController = StoryboardScene.___VARIABLE_productName___.___VARIABLE_productName___ViewController.instantiate()
+        let router = ___VARIABLE_productName___Router(appComponent: appComponent, viewController: viewController)
+        let interactor = ___VARIABLE_productName___Interactor(presenter: viewController, router: router)
+        viewController.interactor = interactor
         
-        return vc
+        return viewController
     }
 }
